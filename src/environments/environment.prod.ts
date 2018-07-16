ee117e7f11ae3382ddaa73432636b41a69221de7
mythcard <mythcard@gmail.com>
2018-07-16 00:58:22 -0500

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAfF9q-eDQD2ZBARKOo4xn9NzFvS8r6AoQ",
+    authDomain: "clientpanelprod-30d9f.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-30d9f.firebaseio.com",
+    projectId: "clientpanelprod-30d9f",
+    storageBucket: "clientpanelprod-30d9f.appspot.com",
+    messagingSenderId: "609208543990"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAfF9q-eDQD2ZBARKOo4xn9NzFvS8r6AoQ",
+    authDomain: "clientpanelprod-30d9f.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-30d9f.firebaseio.com",
+    projectId: "clientpanelprod-30d9f",
+    storageBucket: "clientpanelprod-30d9f.appspot.com",
+    messagingSenderId: "609208543990"
+  }
 };
 
 /*
